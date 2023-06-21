d8eec8029e3522bc618d332de0832f1bc702bb78
vtrungkin13 <vtrungkin13@gmail.com>
2023-06-21 16:55:34 +0700

    add live control

---
+++ Assets/Scripts/LiveController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveController : MonoBehaviour
+{
+    public GameObject liveSprite;
+
+    private GameObject _instantiatedLive;
+
+    [SerializeField]
+    private int maxLives = 3;
+    
+    public int lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = maxLives;
+        Invoke("LivesDisplay", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LivesDisplay()
+    {
+        for (int i = 0; i <= lives; i++)
+        {
+            GameObject liveObject = GameObject.Find("live" + i);
+            if (liveObject != null)
+            {
+                Destroy(liveObject);
+            }
+        }
+            
+        float posX1 = -4.7f;
+        float posX2 = posX1 + 1.2f;
+        float posX3 = posX2 + 1.2f;
+        List<float> list = new List<float>()
+        {
+            posX1, posX2, posX3
+        };
+        float posY = 9f;
+
+        for (int i = 0; i < lives; i++)
+        {
+            _instantiatedLive = Instantiate(liveSprite, new Vector3(list[i], posY, 0), this.transform.rotation);
+            _instantiatedLive.name = "live" + i;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("enemy"))
+        {
+            Destroy(collision.gameObject);
+            lives--;
+            LivesDisplay();
+            if (lives == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
